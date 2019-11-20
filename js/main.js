8c0d71461d0b225b85d58e19c9eeb7e1ce640013
D00194469 <57989712+D00194469@users.noreply.github.com>
2019-11-20 15:07:36 +0000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,55 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+const fill = document.querySelector('.fill'); //Singular element
+const empties = document.querySelectorAll('.empty'); // Node list
+
+//Fill listeners
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+//Loop through empty containers and call drag events
+for (const empty of empties){
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+
+
+//Drag Functions
+function dragStart (){
+    //console.log('start');
+    this.className += ' hold'; //appends
+    setTimeout(() => (this.className = 'invisible'), 0);
+}
+function dragEnd(){
+  //console.log('end');  
+  this.className = 'fill';
+}
+
+function dragOver(e){
+   e.preventDefault();
+   //console.log('over'); 
+}
+
+function dragEnter(e){
+    //console.log('enter');
+    e.preventDefault();
+    this.className+= ' hovered'; ////Appends to it and hover class
+}
+
+function dragLeave(){
+    //console.log('leave');
+    this.className = 'empty'; ////Replaces hover
+}
+
+function dragDrop(){
+    //Console.log('drop');
+    this.className = 'empty'; //
+    this.append(fill);  //Append fill element to empty
+}
