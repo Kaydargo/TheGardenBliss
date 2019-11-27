e2532baa7b3daf4b491c71e55412b2c2266ab7d9
unknown <d00194469@student.dkit.ie>
2019-11-27 13:48:02 +0000

    userprofile page creation and userID call

---
+++ userProfile.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<?php
+require('includes/database.php');
+
+    $queryUser = "SELECT * FROM users WHERE userID = userID"; 
+    $statement2 = $db->prepare($queryUser);
+    $statement2->execute();
+    $users = $statement2->fetchAll();
+    $statement2->closeCursor();
+     ?>
+<head>
+      <title>User Profile</title>
+</head>
+<body> 
+    <h1>User Profile</h1>   
+</body>
+</html>
