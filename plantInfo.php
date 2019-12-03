bc4b1c311d1d470640af3c15206691be86ac9317
Graham <D00194317@student.dkit.ie>
2019-12-03 14:16:42 +0000

    chickens

--- plantInfo.php
+++ plantInfo.php
@@ -6,19 +6,63 @@ and open the template in the editor.
 -->
 <?php
     include('includes/database.php');
-        ?>
+
+//Form Query
+$queryPlant = "SELECT * FROM plant WHERE plantID = '1'";
+$statement1 = $conn->prepare($queryPlant);
+$statement1->execute();
+$plants = $statement1->fetchAll();
+$statement1->closeCursor();
+?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
+    <link href="css/graham.css" rel="stylesheet">
     <?php
     include('includes/header.php');
         ?>
     <body>
-        
+        <br><br><br><br><br>
+    <div class="container"> 
+    <?php foreach ($plants as $plant) : ?>
+   <div class="row row1"> 
+      <div class="col img-responsive">
+        <?php echo "<img class='image1' src='images/".$plant['plantImage']. "' />"; ?>
+      </div>
+      <div class="col">
+          <h3 class="plantName"><?php echo $plant['plantName']; ?></h3>
+          <p><?php echo $plant['description']; ?></p>
+      </div> 
+   </div> 
+</div>
+<div class="container"> 
+   <div class="row">
+<table class ="table table-responsive">
+                <tr class="table-primary">
+                    <th>Soil</th>
+                    <th>Placement</th>
+                    <th>Watering</th>
+                    <th>Distance</th>
+                    <th>Depth</th>
+                    <th>Type</th>
+                </tr>
+                <tr>  
+                    <td><?php echo $plant['soil']; ?></td>
+                    <td><?php echo $plant['placement']; ?></td>
+                    <td><?php echo $plant['watering']; ?></td>
+                    <td><?php echo $plant['distance']; ?></td>
+                    <td><?php echo $plant['depth']; ?></td>
+                    <td><?php echo $plant['type']; ?></td>
+                </tr>
+                
+            </table>
+    </div>
+</div>
+<?php endforeach; ?>
     </body>
-    
+
     <!-- Bootstrap core JavaScript -->
   <script src="vendor/jquery/jquery.min.js"></script>
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
