22215c3f220121a23afaf0ab1848e69e89c87285
Agnese <D00196949@student.dkit.ie>
2019-12-16 12:02:06 +0000

    Plants Page

--- includes/functions.php
+++ includes/functions.php
@@ -1,7 +1,7 @@
 <?php 
 require_once('includes/database.php');
 
-//Display house images in slider
+//Display vegetable images in slider
     $queryVeg = "SELECT * FROM plant WHERE type='Vegetable' LIMIT 4"; 
     $statement1 = $conn->prepare($queryVeg);
     $statement1->execute();
@@ -16,31 +16,31 @@ require_once('includes/database.php');
     $vegetables1 = $statement2->fetchAll();
     $statement2->closeCursor();
 
-//Display garden images in slider
+//Display fruit images in slider
 
-    $queryFruit = "SELECT * FROM plant WHERE type='fruit' LIMIT 4"; 
-    $statement3 = $conn->prepare($queryFruit);
-    $statement3->execute();
-    $fruits = $statement3->fetchAll();
-    $statement3->closeCursor();
-    
-     $queryFruits1 = "SELECT * FROM plant WHERE type='fruit' LIMIT 4,8"; 
-    $statement4 = $conn->prepare($queryFruits1);
-    $statement4->execute();
-    $fruits1 = $statement4->fetchAll();
-    $statement4->closeCursor();
-    
-    
-//Display room images in slider
+$queryFruit = "SELECT * FROM plant WHERE type='fruit' LIMIT 4"; 
+$statement3 = $conn->prepare($queryFruit);
+$statement3->execute();
+$fruits = $statement3->fetchAll();
+$statement3->closeCursor();
 
-    $queryFlower = "SELECT * FROM plant WHERE type='flower' LIMIT 4"; 
-    $statement5 = $conn->prepare($queryFlower);
-    $statement5->execute();
-    $flowers = $statement5->fetchAll();
-    $statement5->closeCursor();
-    
-    $queryFlower1 = "SELECT * FROM plant WHERE type='flower' LIMIT 4,8"; 
-    $statement6 = $conn->prepare($queryFlower1);
-    $statement6->execute();
-    $flowers1 = $statement6->fetchAll();
-    $statement6->closeCursor();
+ $queryFruits1 = "SELECT * FROM plant WHERE type='fruit' LIMIT 4,8"; 
+$statement4 = $conn->prepare($queryFruits1);
+$statement4->execute();
+$fruits1 = $statement4->fetchAll();
+$statement4->closeCursor();
+
+
+//Display room flower in slider
+
+$queryFlower = "SELECT * FROM plant WHERE type='flower' LIMIT 4"; 
+$statement5 = $conn->prepare($queryFlower);
+$statement5->execute();
+$flowers = $statement5->fetchAll();
+$statement5->closeCursor();
+
+$queryFlower1 = "SELECT * FROM plant WHERE type='flower' LIMIT 4,8"; 
+$statement6 = $conn->prepare($queryFlower1);
+$statement6->execute();
+$flowers1 = $statement6->fetchAll();
+$statement6->closeCursor();
--- plants.php
+++ plants.php
@@ -8,22 +8,39 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Freelancer - Start Bootstrap Theme</title>
-
+  <title>Plants Page</title>
+  <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+  <!--Bootstrap core -->
+ 
   <!-- Custom fonts for this theme -->
+  <link href="css/style.css" rel="style">
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
 
   <!-- Theme CSS -->
-  <link href="css/freelancer.min.css" rel="stylesheet">
-  <link href="css/style.css" rel="style">
 
+  
+  <link href="css/freelancer.min.css" rel="stylesheet">
+
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="js/main.js" type="text/javascript"></script>
+  <script src="js/jquery-1.11.3min.js.js" type="text/javascript"></script>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        
 </head>
 
 <body>
-
-
+<?php
+  include('includes/header.php');
+  include('includes/database.php');  
+  ?>
+ <br></br>
+ <br></br>
+ <br></br>
 <?php
 //Selects all images, their id and userid with the tag animated
         require('includes/functions.php');
@@ -39,26 +56,16 @@ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseru
 
 <br><br>
 <br><br><br> 
-            
+ <!--Image slider for Vegetables  -->
 
-            <p class="gal-name gal-buttons" >   <a href="#vegetableImages" class="gallery-nav-links " class="gal-name-link" > Vegetables</a> </p>
-           <p class="gal-name gal-buttons" > <a href="#fruitImages" class="gallery-nav-links gal-name-link"> Fruits</a> </p>
-          <p class="gal-name gal-buttons" >  <a href="#flowerImages" class="gallery-nav-links gal-name-link"> Floweers</a></p>
-
-
-            <br><br>
-            <br><br><br>    
-
-            <!--Image slider for House  -->
-
-            <h2 id="VegImages">Vegetables</h2>
+ <h3 id="VegImages" style="text-align:left;">Vegetables</h3>
             <hr class="divline">
             <div class="container">
                 <div class='row'>
                     <div class='col-md-12'>
                         <div class="carousel slide media-carousel" id="media1">
                             <div class="carousel-inner">
-
+                          
                                 <div class="item active" >
                                     <div class="row"> 
 
@@ -74,9 +81,12 @@ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseru
 
                                             <div class="col-md-3">
     <?= ($veg['plantImage'] <> " " ? "<img style='width:250px; height:250px; margin-top:10px;' src='images/{$veg['plantImage']}'/>" : "") ?>
-
-
-                                            </div>   
+    <div class="carousel-caption">
+                             <p> <a href="plantInfo.php?plantID=<?php echo ($veg['plantID']); ?>"> <?php echo ($veg['plantName']); ?></a></p>
+                             </div>   
+                            
+                                            </div> 
+                                            
 <?php endforeach; ?> 
 
 
@@ -98,8 +108,13 @@ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseru
                                             <div class="col-md-3">
 
                                             <?= ($veg1['plantImage'] <> " " ? "<img style='width:250px; height:250px; margin-top:10px;' src='images/{$veg1['plantImage']}'/>" : "") ?>
+                                            <div class="carousel-caption">
+                                            <p> <a href="plantInfo.php?plantID=<?php echo ($veg1['plantID']); ?>"> <?php echo ($veg1['plantName']); ?></a></p>
+                             </div>   
+                                           
+                                           
+                                            </div>  
 
-                                            </div>   
 <?php endforeach; ?> 
 
                                     </div>
@@ -114,72 +129,80 @@ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseru
                 </div>
             </div>
 
-                <!--Image slider for garden  -->
+                <!--Image slider for Fruits  -->
                 <br><br><br>
 
-                <h2 id="fruitImages">Fruits</h2>
-                <hr class="divline">
-                <div class="container">
-                    <div class='row'>
-                        <div class='col-md-12'>
-                            <div class="carousel slide media-carousel" id="media2">
-                                <div class="carousel-inner">
+
+ <h3 id="FruitImages" style="text-align:left;">Fruits</h3>
+            <hr class="divline">
+            <div class="container">
+                <div class='row'>
+                    <div class='col-md-12'>
+                        <div class="carousel slide media-carousel" id="media2">
+                            <div class="carousel-inner">
+
+                                <div class="item active" >
+                                    <div class="row"> 
 
 
-                                    <div class="item active" >
-                                        <div class="row">
+<?php $counter = 0; ?>
+                                        <?php foreach ($fruits as $fruit): ?>
+                                            <?php
+                                            $counter++; //increase counter number
+                                            if ($counter > 4) {
+                                                break;
+                                            }
+                                            ?>  
 
-                                            <?php $counter3 = 0; ?>
-                                            <?php foreach ($fruits as $fruit): ?>
-                                                    <?php
-                                                    $counter3++; //increase counter number
-                                                    if ($counter3 > 4) {
-                                                        break;
-                                                    }
-                                                    ?>
-                                                <div class="col-md-3">
+                                            <div class="col-md-3">
     <?= ($fruit['plantImage'] <> " " ? "<img style='width:250px; height:250px; margin-top:10px;' src='images/{$fruit['plantImage']}'/>" : "") ?>
+    <div class="carousel-caption">
+    <p> <a href="plantInfo.php?plantID=<?php echo ($fruit['plantID']); ?>"> <?php echo ($fruit['plantName']); ?></a></p>
+                             </div>   
 
-                                                </div>   
+                                            </div>   
 <?php endforeach; ?> 
 
 
-                                        </div>
                                     </div>
-
-
-                                    <div class="item">
-                                        <div class="row">
-
-<?php $counter4 = 0; ?>
-                                            <?php foreach ($fruits1 as $fruit1): ?>
-                                                <?php
-                                                $counter4++; //increase counter number
-                                                if ($counter4 > 4) {
-                                                    break;
-                                                }
-                                                ?><div class="col-md-3">
-    <?= ($fruit1['plantImage'] <> " " ? "<img style='width:250px; height:250px;margin-top:10px;' src='images/{$fruit1['plantImage']}'/>" : "") ?>
-
-                                                </div>   
-<?php endforeach; ?> 
-
-
-                                        </div>
-                                    </div>
-
                                 </div>
-                                <a data-slide="prev" href="#media2" class="left carousel-control">&lt;</a>
-                                <a data-slide="next" href="#media2" class="right carousel-control">&gt;</a>
-                            </div>                          
-                        </div>
+
+
+                                <div class="item">
+                                    <div class="row">
+
+
+                                        <?php $counter1 = 0; ?><?php foreach ($fruits1 as $fruit1): ?>
+                                            <?php
+                                            $counter1++; //increase counter number
+                                            if ($counter1 > 4) {
+                                                break;
+                                            }
+                                            ?>  
+                                            <div class="col-md-3">
+
+                                            <?= ($fruit1['plantImage'] <> " " ? "<img style='width:250px; height:250px; margin-top:10px;' src='images/{$fruit1['plantImage']}'/>" : "") ?>
+                                            <div class="carousel-caption">
+                                            <p> <a href="plantInfo.php?plantID=<?php echo ($fruit1['plantID']); ?>"> <?php echo ($fruit1['plantName']); ?></a></p>
+                             </div> 
+                                            </div>   
+<?php endforeach; ?> 
+
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <a data-slide="prev" href="#media2" class="left carousel-control">&lt;</a>
+                            <a data-slide="next" href="#media2" class="right carousel-control">&gt;</a>
+                        </div>                          
                     </div>
                 </div>
 
-
-                <!--Image slider for Rooms  -->
-                <br><br><br>    
-                <h2 id="roomImages">Flowers</h2>
+            </div>
+ <!--Image slider for Flowers  -->
+ <br><br><br>    
+                <h2 id="roomImages"style= "text-align:left;">Flowers</h2>
                 <hr class="divline">
                 <div class="container">
                     <div class='row'>
@@ -201,7 +224,9 @@ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseru
     ?>
                                                 <div class="col-md-3">
                                                 <?= ($flower['plantImage'] <> " " ? "<img style='width:250px; height:250px; margin-top:10px;' src='images/{$flower['plantImage']}'/>" : "") ?>
-                                                                             
+                                                <div class="carousel-caption">
+                                                <p> <a href="plantInfo.php?plantID=<?php echo ($flower['plantID']); ?>"> <?php echo ($flower['plantName']); ?></a></p>
+                             </div>                              
                                                 </div>   
                                             <?php endforeach; ?> 
 
@@ -222,7 +247,9 @@ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseru
     }
     ?><div class="col-md-3">
     <?= ($flower1['plantImage'] <> " " ? "<img style='width:250px; height:250px; margin-top:10px;' src='images/{$flower1['plantImage']}'/>" : "") ?>
-
+    <div class="carousel-caption">
+    <p> <a href="plantInfo.php?plantID=<?php echo ($flower1['plantID']); ?>"> <?php echo ($flower1['plantName']); ?></a></p>
+                             </div> 
                                                 </div>   
 <?php endforeach; ?> 
 
@@ -239,9 +266,13 @@ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deseru
                 </div>
 
         </div>
+<br><br><br>
+<br> <br>
+
+
+            <script src="js/jQuery.js" type="text/javascript"></script>
                 <script src="vendor/jquery/jquery.min.js"></script>
                 <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-<?php include_once 'includes/footer.php'; ?>
                 </body>
                 </html>
