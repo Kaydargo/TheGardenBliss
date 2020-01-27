adf564e6e270a78604ad85f0c71b1b1f2039b2cc
Graham <D00194317@student.dkit.ie>
2020-01-27 10:10:01 +0000

    footer + homepage

---
+++ includes/footer.php
@@ -0,0 +1,99 @@
+<!-- Footer -->
+<footer class="page-footer font-small primary footCol">
+
+  <!-- Footer Links -->
+  <div class="container">
+
+    <!-- Grid row-->
+    <div class="row text-center d-flex justify-content-center pt-5 mb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-2 mb-3">
+        <h6 class="text-uppercase font-weight-bold">
+          <a href="#!">Homepage</a>
+        </h6>
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-2 mb-3">
+        <h6 class="text-uppercase font-weight-bold">
+          <a href="#!">Plants</a>
+        </h6>
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-2 mb-3">
+        <h6 class="text-uppercase font-weight-bold">
+          <a href="#!">Garden Planner</a>
+        </h6>
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-2 mb-3">
+        <h6 class="text-uppercase font-weight-bold">
+          <a href="#!">How-To</a>
+        </h6>
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-2 mb-3">
+        <h6 class="text-uppercase font-weight-bold">
+          <a href="#!">Bee-Helpful</a>
+        </h6>
+      </div>
+      <div class="col-md-2 mb-3">
+        <h6 class="text-uppercase font-weight-bold">
+          <a href="#!">Login</a>
+        </h6>
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row-->
+    <hr class="rgba-white-light" style="margin: 0 15%;">
+
+    <!-- Grid row-->
+    <!-- Grid row-->
+    <hr class="clearfix d-md-none rgba-white-light" style="margin: 10% 15% 5%;">
+
+    <!-- Grid row-->
+    <div class="row pb-3 socialsFoot">
+
+      <!-- Grid column -->
+      <div class="col-md-12">
+
+        <div class="mb-5 flex-center">
+
+          <!-- Facebook -->
+          <a class="fb-ic">
+            <i class="fab fa-facebook-f fa-lg white-text mr-4"> </i>
+          </a>
+          <!--Instagram-->
+          <a class="ins-ic">
+            <i class="fab fa-instagram fa-lg white-text mr-4"> </i>
+          </a>
+    
+
+        </div>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row-->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3 ccolor">© 2020 Copyright:
+    <a href="> GardenBliss</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
--- index.php
+++ index.php
@@ -9,8 +9,71 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <title></title>
     </head>
-    <body>
-        <?php
+    <?php
+    include('includes/header.php');
         ?>
+    <body>
+    <br><br><br><br>
+    <header class="masthead">
+  <div class="container h-100">
+    <div class="row h-100 align-items-center">
+      <div class="col-12 text-center">
+        <h1 class="font-weight-light">Vertically Centered Masthead Content</h1>
+      </div>
+    </div>
+  </div>
+</header><br><br>
+    <div class="container"> 
+   <div class="row row1"> 
+      <div class="col-sm-6">
+        <img class='image1 img-fluid' src="" alt="">
+      </div>
+      <div class="col-sm-6">
+          <h3 class="plantName">OUR MISSION</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div> 
+   </div> 
+</div>
+<br><br>
+<div class="container"> 
+<h3 class="plantName">Key Functionalities</h3>
+<br>
+<div class="row display-flex">
+<div class="col-sm">
+  <img class="icons img-fluid" src='icons/distance.png'>
+  <h4 class="func">Drag and Drop Garden Planner</h4>
+  </div>
+  <div class="col-sm">
+  <img class="icons img-fluid" src='icons/distance.png'>
+  <h4 class="func">Information Guides on Plant Keeping</h4>
+  </div>
+  <div class="col-sm">
+  <img class="icons img-fluid" src='icons/distance.png'>
+  <h4 class="func">Tutorials on How to Garden</h4>
+  </div>
+  <div class="col-sm">
+  <img class="icons img-fluid" src='icons/distance.png'>
+  <h4 class="func">Interactive Game on Bee Conservation</h4>
+  </div>
+</div> 
+</div>
+<br><br><br>
+<div class="container-fluid ctnCol"> 
+<div class="container pad"> 
+   <div class="row row1"> 
+   <div class="col-sm-6">
+          <h3 class="howTo">About Us</h3>
+          <p class="para"><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div> 
+      <div class="col-sm-6">
+      <img class='image1 img-fluid' src="" alt="">
+      </div>
+   </div> 
+    </div> 
+</div>
+       
     </body>
+    <?php
+    include('includes/footer.php');
+        ?>
 </html>
