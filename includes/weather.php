ffbceb483d159fab6296d9380f2ec86a78a88475
D00194469 <D00194469@users.noreply.github.com>
2020-02-10 16:25:29 +0000

    Weather5days. Edit User profile

--- includes/weather.php
+++ includes/weather.php
@@ -1,48 +1,62 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/weather-icons/2.0.5/css/weather-icons.min.css">
-<div class="container">
-<div class="row">
+<div class="container" id="weather">
+<h4 id="location"></h4><br>
+<div class="row" >
     <div class="card w-lg-20">
       <div class="card-body">
-        <h5 class="card-title weatherTitle" style="text-align: center;">Wed</h5><hr>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <h4 class="card-title weatherTitle" style="text-align: center;"></h4><hr>
+        <h4 class="card-text mainTemp">Main Temperture</h4>
+        <p class="card-text icon">Icon</p>
+        <h6 class="card-text weather">Weather type.</h6>
+        <p class="card-text temp">Min, Max temp</p>
       </div>
   </div>
-    <div class="card w-lg-20">
-      <div class="card-body">
-        <h5 class="card-title weatherTitle" style="text-align: center;">Thu</h5><hr>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-      </div>
-  </div>
-    <div class="card w-lg-20">
-      <div class="card-body">
-        <h5 class="card-title weatherTitle" style="text-align: center;">Fri</h5><hr>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-      </div>
-  </div>
-    <div class="card w-lg-20">
-      <div class="card-body">
-        <h5 class="card-title weatherTitle" style="text-align: center;">Sat</h5><hr>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-      </div>
-    </div>
-    <div class="card w-lg-20">
-      <div class="card-body">
-        <h5 class="card-title weatherTitle" style="text-align: center;">Sun</h5><hr>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-      </div>
-    </div>
-</div>
-</div>
 
-<div id="weather"><br>
-     <h1>Todays Weather</h1>
-     <h4 id="location"></h4>
-<div id="icon"><img id="wicon" src="" alt="Weather icon"/></div>
+    <div class="card w-lg-20">
+      <div class="card-body">
+        <h4 class="card-title weatherTitle" style="text-align: center;"></h4><hr>
+        <h4 class="card-text mainTemp">Main Temperture</h4>
+        <p class="card-text icon">Icon</p>
+        <h6 class="card-text weather">Weather type.</h6>
+        <p class="card-text temp">Min, Max temp</p>
+      </div>
+  </div>
+
+    <div class="card w-lg-20">
+      <div class="card-body">
+        <h4 class="card-title weatherTitle" style="text-align: center;"></h4><hr>
+        <h4 class="card-text mainTemp">Main Temperture</h4>
+        <p class="card-text icon">Icon</p>
+        <h6 class="card-text weather">Weather type.</h6>
+        <p class="card-text temp">Min, Max temp</p>
+      </div>
+  </div>
+
+    <div class="card w-lg-20">
+      <div class="card-body">
+        <h4 class="card-title weatherTitle" style="text-align: center;"></h4><hr>
+        <h4 class="card-text mainTemp">Main Temperture</h4>
+        <p class="card-text icon">Icon</p>
+        <h6 class="card-text weather">Weather type.</h6>
+        <p class="card-text temp">Min, Max temp</p>
+      </div>
+    </div>
+
+    <div class="card w-lg-20">
+      <div class="card-body">
+        <h4 class="card-title weatherTitle" style="text-align: center;"></h4><hr>
+        <h4 class="card-text mainTemp">Main Temperture</h4>
+        <p class="card-text icon">Icon</p>
+        <h6 class="card-text weather">Weather type.</h6>
+        <p class="card-text temp">Min, Max temp</p>
+      </div>
+    </div>
+
+<!-- <div id="icon"><img id="wicon" src="" alt="Weather icon"/></div>
     <div id="description"></div>
     <span id="mintemp"></span>&nbsp;&nbsp;
- <span id="maxtemp"></span><br><br>
-  <!-- Displays longitude and latitude using geolocation -->
-  <!-- <button onclick="getLocation()">Get weather for current location</button>
-  <p id="getLocation"></p> -->
+ <span id="maxtemp"></span><br><br> -->
 
+  </div>
+</div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
