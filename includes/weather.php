10e82b5bfc953a7ee8453251f7a346db2660f93a
D00194469 <D00194469@users.noreply.github.com>
2020-03-22 18:38:05 +0000

    gardenplanner move to current file

--- includes/weather.php
+++ includes/weather.php
@@ -59,5 +59,9 @@
     </div> 
   </div><br>
 <!-- </div> -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<!-- Additional css and js files are required on this page to overwrite code in 3.4.1 jquery.min.js which breaks drag/drop functionality -->
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+ <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script src="../js/weatherBit.js"></script>
