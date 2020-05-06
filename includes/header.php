d8340f599637489697d53224ddb7bfd67a6fafb6
D00194469 <D00194469@users.noreply.github.com>
2020-05-06 18:26:07 +0100

    updated Logo

--- includes/header.php
+++ includes/header.php
@@ -14,7 +14,7 @@
  <!-- Navigation -->
  <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
     <div class="container">
-      <a href="index.php"><img id="logo" src="images/logo-white.png" alt='the garden bliss'></a>
+      <a href="index.php"><img id="logo" src="images/logo-white-text.png" alt='the garden bliss'></a>
       <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i class="fas fa-bars"></i>
